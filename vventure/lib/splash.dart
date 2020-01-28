827c182425a7563e3b818421e345b76bfedeadb6
Daniel Cabrera <daniel.cabrera.checa@estud.usfq.edu.ec>
2020-01-27 21:33:31 -0500

    splash screen added

---
+++ vventure/lib/splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:vventure/login/view/login_view.dart';
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: new LoginView(),
+        image: new Image.asset('assets/images/vventure.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.deepPurpleAccent);
+  }
+}
